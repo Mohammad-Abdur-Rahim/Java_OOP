1d3321062d0584aa8bc596d07ea90e1fa335a3d7
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-02-11 12:11:42 +0600

    Data-type Declare java

---
+++ Learn _java/src/java_learn_daffodil_university/Data_type.java
@@ -0,0 +1,24 @@
+package java_learn_daffodil_university;
+
+public class Data_type {
+    public static void main(String[] args) {
+        float f=3.05f;
+        char c='R';
+        boolean b= true;
+        int id=5915;
+        String title= "iphone15";
+        String price="1895 Euros ";
+        String description="perfect product with best image quality";
+        String category="phone";
+        System.out.println("ID:"+id);
+        System.out.println("Price"+price);
+        System.out.println("Description:"+description);
+        System.out.println("Category:"+category);
+        System.out.println("Cgpa Is :"+f);
+        System.out.println("Char is :"+c);
+        System.out.println("Boolean:"+b);
+        // just print this line same work easy to handale programe
+        System.out.println("ID:"+id+"\n"+"Price"+price+"\n"+"Description:"+description+"\n"+"Category:"+category);
+        // just print this line same work easy to handale programe
+    }
+}
