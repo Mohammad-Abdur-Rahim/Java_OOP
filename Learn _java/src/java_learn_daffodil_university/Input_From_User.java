d3363edd0bc856a26a8b8b3210ced66daafa9c4b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-02-11 20:23:21 +0600

    Scanner_from user Assignment

--- Learn _java/src/java_learn_daffodil_university/Input_From_User.java
+++ Learn _java/src/java_learn_daffodil_university/Input_From_User.java
@@ -3,11 +3,48 @@ import java.util.Scanner;
 
 public class Input_From_User {
     public static void main(String[] args) {
-  String name;
-  Scanner input=new Scanner(System.in);   //1st steap Scanner declare
+    int id,price;
+    String tittle,description,category;
 
-        System.out.println("Enter your name:");
-        name=input.nextLine();             // 2nd steap user value declare
-        System.out.println("Name Is:"+name);  // string = next() vs nextline()................important
+
+    //User Id input coding.................
+    Scanner input_id = new Scanner(System.in);
+    System.out.print("Enter your Id :");
+    id=input_id.nextInt();
+    System.out.println("Id ="+id);
+    //User Id input coding end.............
+
+
+
+
+        //User Price input coding.................
+        Scanner input_price = new Scanner(System.in);
+        System.out.print("Enter your Price :");
+        price=input_price.nextInt();
+        System.out.println("Price ="+price);
+        //User Price input coding end.............
+
+
+
+        //User Tittle input coding.................
+        Scanner input_tittle = new Scanner(System.in);
+        System.out.print("Enter your Tittle :");
+        tittle=input_tittle.nextLine();
+        System.out.println("Tittle ="+tittle);
+        //User Tittle input coding end.............
+
+        //User Description input coding.................
+        Scanner input_description = new Scanner(System.in);
+        System.out.print("Enter your Description :");
+        description=input_description.nextLine();
+        System.out.println("Description ="+description);
+        //User Description input coding end.............
+
+        //User Category input coding.................
+        Scanner input_category = new Scanner(System.in);
+        System.out.print("Enter your Category :");
+        category=input_category.nextLine();
+        System.out.println("Category ="+category);
+        //User Category input coding end.............
     }
 }
