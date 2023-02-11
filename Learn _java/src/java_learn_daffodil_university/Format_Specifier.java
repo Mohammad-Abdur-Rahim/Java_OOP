0fd60957a6803088b9257e015c1bbcedaa7b3d3f
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-02-11 12:26:58 +0600

    java_Format_Specifier

---
+++ Learn _java/src/java_learn_daffodil_university/Format_Specifier.java
@@ -0,0 +1,16 @@
+package java_learn_daffodil_university;
+
+public class Format_Specifier {
+    public static void main(String[] args) {
+        int i=10;
+        short s=9;
+        char c='R';
+        double d= 5915;
+        boolean b=true;
+        System.out.printf("int vale =%d",i);
+        System.out.printf("short =%d",s);
+        System.out.printf("Char =%c",c);
+        System.out.printf("Double=%f",d);
+        System.out.printf("boolean=%b",b);
+    }
+}
