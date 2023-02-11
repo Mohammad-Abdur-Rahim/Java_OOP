369581388e218b7320fb9e96da9401f340bcd621
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-02-11 12:30:47 +0600

    First-java programe update

--- Learn _java/src/java_learn_daffodil_university/Main_Class.java
+++ Learn _java/src/java_learn_daffodil_university/Main_Class.java
@@ -1,5 +1,14 @@
 package java_learn_daffodil_university;
 
 public class Main_Class {
-
+    public static void main(String[] args) {
+        System.out.println("First_print My Java programming Language");
+        System.out.println("Department: Computer science & Engineering");
+        System.out.println("Name: MD. ABDUR RAHIM");
+        System.out.println("Id:221-15-5915");
+        System.out.println("Section: 'V'");
+        System.out.println("Cgpa: 3.42");
+        System.out.println("Dream: Web,Software & Cyber security Developer");
+    }
 }
+
