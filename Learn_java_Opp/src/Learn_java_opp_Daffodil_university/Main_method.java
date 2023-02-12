e6c605c12acd043bce11eef51edbdd1d9eb53dc8
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-02-12 11:20:28 +0600

    Constructor Use Object Value Assign

--- Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Main_method.java
+++ Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Main_method.java
@@ -3,14 +3,11 @@ package Learn_java_opp_Daffodil_university;
 public class Main_method {
     public static void main(String[] args) {
 
-        Teacher teacher1=new Teacher(); // object create Teacher class
-        teacher1.setInformation("Md Abdur Rahim","Male",21);
+        Teacher teacher1=new Teacher("Md Abdur Rahim","Male",21); // Constructor assign vale.........
         teacher1.display(); // Methos Declare with object. here......
 
-        Teacher teacher2=new Teacher(); // object create Teacher class
-        teacher2.setInformation(" Ms Fatema","Female",18);
-        teacher2.display(); // Methos Declare with object. here......
-
+        Teacher teacher2=new Teacher(" Ms Fatema","Female",18); // Constructor assign vale..........display(); // Methos Declare with object. here......
+        teacher2.display();
 
     }
 }
