70519c8397c1db5a18ec56667f84935f5fa6781f
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-02-12 06:57:15 +0600

    just create Display vale box

--- Learn _java/src/java_learn_daffodil_university/Box_assignment.java
+++ Learn _java/src/java_learn_daffodil_university/Box_assignment.java
@@ -3,14 +3,7 @@ package java_learn_daffodil_university;
 
     public class Box_assignment {
         static int height,width, length;
-        static int area(int height, int width) {  //1st method declare Area calculate here..........
-            return height * width;
-        }
 
-        static int area2(int height, int width, int length) { //2nd method declare calculate here..........
-            return height * width * length;
-
-        }
 
         static int display() {                         //3rd  method declare here only print vale..........
             System.out.println(height);
@@ -19,12 +12,16 @@ package java_learn_daffodil_university;
 
             return 0;
         }
+
+
         public static void main(String[] args) {
+
             Box_assignment box = new Box_assignment();
             box.height = 221;
             box.width = 15;
             box.length = 5915;
             display();  // Display method declare and print vale all......
+
 
 
     }
