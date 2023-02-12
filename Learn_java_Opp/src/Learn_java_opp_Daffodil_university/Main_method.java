65fc594764ccf6c1eeff9462d05faff376bbd5ce
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-02-12 07:15:58 +0600

    Declare class_&_Object

---
+++ Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Main_method.java
@@ -0,0 +1,22 @@
+package Learn_java_opp_Daffodil_university;
+
+public class Main_method {
+    public static void main(String[] args) {
+
+        Teacher teacher1=new Teacher(); // object create Teacher class
+        teacher1.name="Md Abdur Rahim";
+        teacher1.gender="Male";
+        teacher1.age=21;
+        
+        System.out.println("Name:"+teacher1.name);
+        System.out.println("Gender:"+teacher1.gender);
+        System.out.println("Age:"+teacher1.age);
+
+
+
+
+
+    }
+}
+
+
---
+++ Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Teacher.java
@@ -0,0 +1,7 @@
+package Learn_java_opp_Daffodil_university;
+
+public class Teacher {  // How to Declare (Class to Object).....................
+    String name,gender; // variable assign only
+    int age;
+
+}
