ebec5d26ecac5d46e23b18b26f158524efd05fab
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-04-02 22:34:22 +0600

    ** Declare static **

--- After_MIdterm/src/Spring23/Main.java
+++ After_MIdterm/src/Spring23/Main.java
@@ -2,9 +2,12 @@ package Spring23;
 
 public class Main {
     public static void main(String[] args){
-Static obj1=new Static("Md abdur rahim","Male");
-obj1.display(); // auto print static
 
+Static obj1=new Static("Md abdur rahim","Male");
+Static obj2=new Static("Md ","Male");
+
+        obj1.display(); // auto print static
+        obj2.display();
     }
 }
 
