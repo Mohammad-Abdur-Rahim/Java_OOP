94aff34a9a283bb311f8889068cffe3ab2ebc9bc
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-04-02 21:52:06 +0600

    *** Default & Parametr Constructor ***

--- After_MIdterm/src/Spring23/Main.java
+++ After_MIdterm/src/Spring23/Main.java
@@ -1,5 +1,18 @@
 package Spring23;
 
 public class Main {
+    public static void main(String[] args) {
+
+
+        Teacher t1=new Teacher("Rahim","male",99); // pass value constructor
+        t1.display();
+
+        Teacher t2=new Teacher("sakib","male",111);
+        t2.display();
+
+        Teacher t3=new Teacher(); //default constructor
+        t3.display();
+
+    }
 
 }
