6ddcabe7cfe8890ce9e674c932d2a7b79e9f884d
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-04-02 22:11:18 +0600

    *** Return Value Method ***

--- After_MIdterm/src/Spring23/Main.java
+++ After_MIdterm/src/Spring23/Main.java
@@ -4,15 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Teacher t1=new Teacher("Rahim","male",99); // pass value constructor
-        t1.display();
-
-        Teacher t2=new Teacher("sakib","male",111);
-        t2.display();
-
-        Teacher t3=new Teacher(); //default constructor
-        t3.display();
-
+ReturnValueMethod obj1=new ReturnValueMethod();
+int result = obj1.squre(6);
+System.out.println(result);
     }
 
 }
---
+++ After_MIdterm/src/Spring23/ReturnValueMethod.java
@@ -0,0 +1,8 @@
+package Spring23;
+
+public class ReturnValueMethod {
+
+    int squre(int x){
+    return  x*x;
+    }
+}
