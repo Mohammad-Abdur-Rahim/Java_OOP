3c536ac3079058f482c181c0fac7eee644b34246
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-04-05 13:02:48 +0600

    ** Math Method All **

---
+++ Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Math_demo.java
@@ -0,0 +1,17 @@
+package Learn_java_opp_Daffodil_university;
+
+public class Math_demo {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-9.8));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.pow(5,5));
+        System.out.println(Math.log(2.0));
+        System.out.println(Math.exp(1.0));
+        System.out.println(Math.max(-9,8));
+        System.out.println(Math.min(-9,8));
+        System.out.println(Math.ceil(-9.8));
+        System.out.println(Math.floor(-9.8));
+    }
+}
