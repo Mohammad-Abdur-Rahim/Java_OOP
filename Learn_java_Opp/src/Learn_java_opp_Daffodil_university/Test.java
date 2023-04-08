c0d59240b88f40f866e60fc31b9e6180624cbd62
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-04-08 22:06:11 +0600

    ***  Polymorphism Dynamic referance 02 **

--- Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Test.java
+++ Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Test.java
@@ -2,11 +2,11 @@ package Learn_java_opp_Daffodil_university;
 
 public class Test {
     public static void main(String[] args) {
-        Shape s=new Shape();
-        Rectangle r=new Rectangle(10,20);
-        Triangle t=new Triangle(10,33);
-        System.out.println(s.area());
-        System.out.println(r.area());
-        System.out.println(t.area());
+        Shape s1=new Shape();                   //Dynamic way polymorphism
+        Shape s2=new Rectangle(10,20);       //Dynamic way polymorphism
+        Shape s3=new Triangle(10,33);    //Dynamic way polymorphism
+        System.out.println(s1.area());
+        System.out.println(s2.area());
+        System.out.println(s3.area());
     }
 }
