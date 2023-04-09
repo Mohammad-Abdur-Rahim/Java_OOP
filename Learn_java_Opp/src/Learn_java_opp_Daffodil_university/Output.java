fba40530e4d0568fa56e0bbe4a8575eea37a1b98
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-04-09 16:34:26 +0600

    *** 51 Output Trace 02 **

--- Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Output.java
+++ Learn_java_Opp/src/Learn_java_opp_Daffodil_university/Output.java
@@ -2,19 +2,38 @@ package Learn_java_opp_Daffodil_university;
 
 public class Output {
 
-    public static void main(String[] args) {
-        try {
-            System.out.printf("1");
-            int sum = 9 / 0;
-            System.out.printf("2");
-        } catch (ArithmeticException e) {
-            System.out.printf("3");
-        } catch (Exception e) {
+    private void m1()
+    {
+        m2();
+        System.out.printf("1");
+    }
+    private void m2()
+    {
+        m3();
+        System.out.printf("2");
+    }
+    private void m3()
+    {
+        System.out.printf("3");
+        try
+        {
+            int sum = 4/0;
             System.out.printf("4");
-        } finally {
+        }
+        catch(ArithmeticException e)
+        {
             System.out.printf("5");
         }
+
+
+        System.out.printf("7");
     }
-
-
+    public static void main(String[] args)
+    {
+        Output obj = new Output();
+        obj.m1();
+    }
 }
+
+
+
