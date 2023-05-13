988feed84175e32bbe9e6294f7891b8d7b385b74
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-05-13 23:29:20 +0600

    ** Multiple Interface **

---
+++ Learn_java_Opp/src/Interface/C.java
@@ -0,0 +1,8 @@
+package Interface;
+
+public class C implements A,B{
+    @Override
+    public void play() {
+        System.out.println("Play c");
+    }
+}
--- Learn_java_Opp/src/Interface/Main.java
+++ Learn_java_Opp/src/Interface/Main.java
@@ -2,9 +2,7 @@ package Interface;
 
 public class Main {
     public static void main(String[] args) {
-    Dog obj = new Dog();
-    obj.eat();
-    Cat obj1= new Cat();
-    obj1.eat();
+    C obj= new C();
+    obj.play();
     }
 }
