60f6dee6647ae69f772a9f6d78dac88454d17414
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-05-27 21:25:00 +0600

    02 Lab Report

---
+++ Learn_java_Opp/src/Simpleobject.java
@@ -0,0 +1,9 @@
+public class Simpleobject {
+    public Simpleobject() {
+        System.out.println("Hello, simple object is created");
+    }
+
+    public static void main(String[] args) {
+        Simpleobject obj = new Simpleobject();
+    }
+}
