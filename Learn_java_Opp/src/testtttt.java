a6f64b5fcbaa645b695ff6f916c8313f11eeb8d5
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-06-23 10:24:28 +0600

    nothing

---
+++ Learn_java_Opp/src/testtttt.java
@@ -0,0 +1,9 @@
+public class testtttt {
+    public static void main(String[] args)
+        {
+            int arr[] = {1, 2, 3, 4, 5};
+            for (int i = 0; i <= 5; i++) {
+                System.out.print("Array elements are : " + arr[i] + "\n");
+            }
+        }
+    }
